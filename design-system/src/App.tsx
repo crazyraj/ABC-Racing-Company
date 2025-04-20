40dbec19b0a68cb56a1dbfc8697c0b5b04d84122
Rajkumar <touchwithraj@gmail.com>
2025-04-20 10:55:22 +0530

    Design system project

---
+++ design-system/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export { default as designSystemTheme } from './theme';
+export { default as Dropdown } from './components/Dropdown';
+
+function App() {
+	return <p>Design System</p>;
+}
+
+export default App;
---
+++ design-system/src/theme.ts
@@ -0,0 +1,20 @@
+const theme = {
+    US:{
+        header: {
+            backgroundColor: "rgb(241, 14, 14)"
+        },
+        footer: {
+            backgroundColor: "rgb(190, 186, 186)"
+        }
+    },
+    IN: {
+        header: {
+            backgroundColor: "rgb(81, 14, 241)"
+        },
+        footer: {
+            backgroundColor: "rgb(71, 71, 71)"
+        }
+    }
+}
+
+export default theme;
